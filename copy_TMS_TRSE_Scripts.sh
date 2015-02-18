3babb70d3b6de858eeb0f6d1f1dff0a000dc7913
kai.hwang@gmail.com <kaihwang@nx4.priv>
2015-02-18 13:02:48 -0800

    add timing logs from TMS TRSE data (sub 106 to 118)

---
+++ copy_TMS_TRSE_Scripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# copy over Taraz's TMS dataset.
+
+
+for s in 106 107 108 109 110 111 112 113 114 115 116 117 118; do
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/both_faces.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_both_face.txt
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/both_scenes.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_both_scene.txt
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/categorize_faces.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_categorize_faces.txt
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/categorize_scenes.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_categorize_scenes.txt
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/relevant_faces.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_relevant_faces.txt
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/relevant_scenes.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_relevant_scenes.txt
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/irrelevant_faces.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_irrelevant_faces.txt
+	cp /home/despoB/kaihwang/TRSETMS/${s}/PRE/behavior/irrelevant_scenes.txt /home/despoB/kaihwang/TRSE/TRSE_scripts/${s}_irrelevant_scenes.txt
+
+done
